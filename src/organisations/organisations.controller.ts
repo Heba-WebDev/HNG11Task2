9c97325c461bd86d5f6206f7ae4d361c2bf11745
Heba-Webdev23 <jtvonline@yahoo.com>
2024-07-07 02:18:40 +0300

    feat: added controllers to creat an org, add a member to an org, get all orgs of a user and get an org by id

---
+++ src/organisations/organisations.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { OrgAuthGuard } from './guards/org-auth.guard';
+import { OrganisationsService } from './organisations.service';
+import { CreateOrgDto } from './dtos';
+import { AddMemberDto } from './dtos/add-member.dto';
+
+type Obj = {
+  userId: string;
+};
+
+@Controller('')
+export class OrganisationsController {
+  constructor(private readonly orgsService: OrganisationsService) {}
+
+  @UseGuards(OrgAuthGuard)
+  @Post('/api/organisations')
+  async create(@Body() orgDto: CreateOrgDto, @Body() userId: Obj) {
+    return this.orgsService.create(orgDto, userId);
+  }
+
+  @Post('/api/organisations/:orgId/users')
+  async addMember(@Param('orgId') orgId: string, @Body() dto: AddMemberDto) {
+    return this.orgsService.addMember(orgId, dto.userId);
+  }
+
+  @UseGuards(OrgAuthGuard)
+  @Get('/api/organisations')
+  async getAll(@Body() userId: Obj) {
+    return this.orgsService.getAll(userId);
+  }
+
+  @UseGuards(OrgAuthGuard)
+  @Get('/api/organisations/:orgId')
+  async getById(@Param('orgId') id: string, @Body() userId: Obj) {
+    return this.orgsService.getById(id, userId);
+  }
+}
