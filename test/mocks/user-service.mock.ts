867958f462ba3fa2202dccf8285594fd0f64c6aa
Heba-Webdev23 <jtvonline@yahoo.com>
2024-07-06 23:17:53 +0300

    feat: user service mock

---
+++ test/mocks/user-service.mock.ts
@@ -0,0 +1,10 @@
+export const userServiceMock = {
+  register: jest.fn(),
+  login: jest.fn(),
+  get: jest.fn(),
+  emailExists: jest.fn(),
+  userExists: jest.fn(),
+  viewAuth: jest.fn(),
+  createOrg: jest.fn(),
+  createFounder: jest.fn(),
+};
