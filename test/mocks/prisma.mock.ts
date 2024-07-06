838d4e416217804c89a5a4ba80eb1ebe0c226c44
Heba-Webdev23 <jtvonline@yahoo.com>
2024-07-06 23:17:15 +0300

    feat: prisma mock

---
+++ test/mocks/prisma.mock.ts
@@ -0,0 +1,14 @@
+export const prismaMock = {
+  user: {
+    findFirst: jest.fn(),
+    create: jest.fn(),
+  },
+  organisation: {
+    create: jest.fn(),
+    findFirst: jest.fn(),
+  },
+  userOrganisation: {
+    create: jest.fn(),
+    findFirst: jest.fn(),
+  },
+};
