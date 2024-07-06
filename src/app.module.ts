37c9674ebad420e9c2187a4327fa585b90d5bd7f
Heba-Webdev23 <jtvonline@yahoo.com>
2024-07-07 02:17:29 +0300

    feat: added OrganisationsModule to imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { UsersModule } from './users/users.module';
+import { OrganisationsModule } from './organisations/organisations.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, OrganisationsModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
